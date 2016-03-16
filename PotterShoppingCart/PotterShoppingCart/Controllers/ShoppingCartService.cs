83454d68618b10de1a2743ccf645b51bc0429f13
TernenceZheng <andy711023@hotmail.com>
2016-03-16 16:29:54 +0800

    Scenario: 第一集買了一本，第二集也買了一本，價格應為100*2*0.95=190---ProductionCode完成

--- PotterShoppingCart/PotterShoppingCart/Controllers/ShoppingCartService.cs
+++ PotterShoppingCart/PotterShoppingCart/Controllers/ShoppingCartService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using PotterShoppingCart.Models;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace PotterShoppingCart.Controllers
 {
@@ -10,12 +11,37 @@ namespace PotterShoppingCart.Controllers
         {
             double totoalAmount = 0;
 
-            foreach (BookOrders item in bookOrders)
+            //最後折扣額度
+            double discount = 0;
+
+            //總共有多少本不同的書冊
+            int diffBookNum = 0;
+
+            diffBookNum = bookOrders.Select(x => x.BookName).Distinct().Count();
+
+            //兩本不同的書，則會有5%的折扣
+            switch (diffBookNum)
             {
-                totoalAmount = item.Amount * item.Number;
+                case 2:
+                    discount = 0.95;
+                    break;
+                case 3:
+                    discount = 0.90;
+                    break;
+                default:
+                    break;
             }
 
-            totoalAmount = totoalAmount * 0.95;
+            foreach (BookOrders item in bookOrders)
+            {
+                totoalAmount += item.Amount * item.Number;
+            }
+
+            if (discount > 0)
+            {
+                totoalAmount = totoalAmount * discount;
+            }
+
 
             return totoalAmount;
         }
