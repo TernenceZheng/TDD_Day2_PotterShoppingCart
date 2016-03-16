7bc6631456df9a4be3e9ead60ec65931881e0e1c
TernenceZheng <andy711023@hotmail.com>
2016-03-17 00:13:19 +0800

    Scenario: 一二集各買了一本，第三集買了兩本，價格應為100*3*0.9 + 100 = 370---測試案例完成(綠燈)

--- PotterShoppingCart/PotterShoppingCart/Controllers/ShoppingCartService.cs
+++ PotterShoppingCart/PotterShoppingCart/Controllers/ShoppingCartService.cs
@@ -11,13 +11,50 @@ namespace PotterShoppingCart.Controllers
         {
             double totoalAmount = 0;
 
-            //最後折扣額度
+            //取出最多訂購的數量
+            int bigOrderNum = bookOrders.Max(c => c.Number);
+
             double discount = 0;
+
+            for (int i = bigOrderNum; i >= 1; i--)
+            {
+                discount = GetDiscountType(i, bookOrders);
+
+                totoalAmount += CalAmount(discount , i, bookOrders );
+            }
+
+            return totoalAmount;
+        }
+
+        private double CalAmount(double discount ,int bigOrderNum, List<BookOrders> bookOrders)
+        {
+
+            List<BookOrders> groupBookOrder = bookOrders.Where(y => y.Number >= bigOrderNum).ToList();
+
+            double totoalAmount = 0;
+
+            foreach (BookOrders item in groupBookOrder)
+            {
+                totoalAmount += item.Amount *  1;
+            }
+
+            if (discount > 0)
+            {
+                totoalAmount = totoalAmount * discount;
+            }
+
+            return totoalAmount;
+        }
+
+        private double GetDiscountType(int bigOrderNum, List<BookOrders> bookOrders)
+        {
+            //最後折扣額度
+            double discount = 1;
 
             //總共有多少本不同的書冊
             int diffBookNum = 0;
 
-            diffBookNum = bookOrders.Select(x => x.BookName).Distinct().Count();
+            diffBookNum = bookOrders.Where(y => y.Number >= bigOrderNum).Select(x => x.BookName).Distinct().Count();
 
             //兩本不同的書，則會有5%的折扣
             switch (diffBookNum)
@@ -38,18 +75,7 @@ namespace PotterShoppingCart.Controllers
                     break;
             }
 
-            foreach (BookOrders item in bookOrders)
-            {
-                totoalAmount += item.Amount * item.Number;
-            }
-
-            if (discount > 0)
-            {
-                totoalAmount = totoalAmount * discount;
-            }
-
-
-            return totoalAmount;
+            return discount;
         }
     }
 }
