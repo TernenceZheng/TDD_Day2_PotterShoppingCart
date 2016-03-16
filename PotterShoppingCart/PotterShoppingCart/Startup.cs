bf5fd559ffe9248342369bf2ffdefcddaf3a283a
TernenceZheng <andy711023@hotmail.com>
2016-03-16 13:25:41 +0800

    建立專案

---
+++ PotterShoppingCart/PotterShoppingCart/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PotterShoppingCart.Startup))]
+namespace PotterShoppingCart
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
